c6e1dfc07cb108e307d1186f92e00f8730fc0e33
Valhalla-Wait <60570103+Valhalla-Wait@users.noreply.github.com>
2022-10-15 18:11:22 +0300

    common_bundle

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style/style.scss';
+
+const say = () => console.log('hello')
+say()
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Импортируем плагин
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development'; // По умолчанию режим development
+let target = 'web'
+
+if (process.env.NODE_ENV === 'production') { // Режим production, если 
+  // при запуске вебпака было указано --mode=production
+  mode = 'production';
+  target = 'browserslist'
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html', // Данный html будет использован как шаблон
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }),
+]; // Создаем массив плагинов
+
+module.exports = {
+  mode,
+  target,
+  plugins,
+  entry: './src/index.js', // Указываем точку входа - главный модуль приложения,
+  // в который импортируются все остальные
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Директория, в которой будет
+    // размещаться итоговый бандл, папка dist в корне приложения
+    clean: true, // Очищает директорию dist перед обновлением бандла
+    // Свойство стало доступно с версии 5.20.0, до этого использовался
+    // CleanWebpackPlugin
+  },
+  devtool: 'source-map',
+
+  devServer: {
+    hot: true, // Включает автоматическую перезагрузку страницы при изменениях
+  },
+  module: {
+  	rules: [
+      { test: /\.(html)$/, use: ['html-loader'] }, // Добавляем загрузчик для html
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }, // Добавляем загрузчики стилей
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+        // изображения размером до 8кб будут инлайнится в код
+        // В режиме разработки все изображения будут помещаться в dist/assets
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // не обрабатываем файлы из node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true, // Использование кэша для избежания рекомпиляции
+            // при каждом запуске
+          },
+        },
+      },
+    ],
+  }
+}
